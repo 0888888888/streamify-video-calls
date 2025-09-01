3207dc0158e764b0a46745e839060983ce7097ab
0888888888 <anshuraj9905151156@gmail.com>
2025-09-01 18:22:35 +0530

    change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://streamify-video-calls-a0ni.onrender.com/api" : "/api";
+const BASE_URL = "https://streamify-video-calls-a0ni.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
