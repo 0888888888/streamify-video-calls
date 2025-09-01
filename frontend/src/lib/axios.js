5934b45dc944b8493f6e335dc582d474188331d1
0888888888 <anshuraj9905151156@gmail.com>
2025-09-01 17:50:23 +0530

    apiii

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api"
-    : import.meta.env.VITE_API_BASE_URL || "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://streamify-video-calls-a0ni.onrender.com/api" : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true,
+  withCredentials: true, // send cookies with the request
 });
